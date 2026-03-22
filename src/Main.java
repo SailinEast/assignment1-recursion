660de17bd237b122dd17e30d75f5fbf99a109713
Sail <anasboranbaev@gmail.com>
2026-03-22 19:49:10 +0500

    task: 10th task

--- src/Main.java
+++ src/Main.java
@@ -69,7 +69,7 @@ public class Main {
                 System.out.println("--- Task 9: Count Characters in a String ---");
                 System.out.print("Enter a string: ");
                 String task9Str = scanner.next();
-                System.out.println(countCharacters(task9Str));
+                System.out.println(countCharacters(task9Str, 0));
                 break;
             case 10:
                 System.out.println("--- Task 10: Greatest Common Divisor (GCD) ---");
@@ -152,14 +152,14 @@ public class Main {
     }
 
     // Task 9
-    public static int countCharacters(String s) {
-        // TODO: Implement recursive logic to count characters
-        return 0;
+    public static int countCharacters(String s, int i) {
+        if (s.length() == i) return 0;
+        return 1 + countCharacters(s, i + 1);
     }
 
     // Task 10
     public static int findGCD(int a, int b) {
-        // TODO: Implement recursive logic using Euclidean Algorithm
-        return 0;
+        if (b == 0) return a;
+        return findGCD(b, a % b);
     }
 }
